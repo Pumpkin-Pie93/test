fa6522560802165f97eac2d587856d504fb089e7
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2024-06-08 16:51:01 +0300

    add new component colorLine

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ColorLine from "./components/colorLine.tsx";
 
 function App() {
 
   return (
     <div>
-    <h1>Hello</h1>
+    <h1>Hello!!</h1>
+      <ColorLine color={'red'}/>
     </div>
   )
 }
